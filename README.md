cceb30cebccad22a71c177b2bef74b42c498417c
Jonathan Blood <JonathanBlood2009@gmail.com>
2017-01-07 17:45:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Electron Simple Desktop Notifier
+
+A simple way to POST desktop notifications via a REST API.
+The application is build using Electron.
+
+## How to run apps
+
+1. Install Electron via `npm install -g electron@1.4.13`.
+
+2. Navigate into project directory `cd electron-simple-notifier`
+
+3. Download Project dependencies `npm i`
+
+2. Run the sample via `electron electron-simple-notifier`.
+
+## License
+
+electron-sample-apps is published under the Apache v2 license. See `LICENSE` for details.
---
+++ settings.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const remote = require('electron').remote;
+var window = remote.getCurrentWindow();
+
+function doNotify(evt) {
+    if (evt.srcElement.id == "save") {
+        var port = document.getElementById("port").value;
+        remote.getGlobal('settings').port = port;
+        var ipcRenderer = require('electron').ipcRenderer;
+        ipcRenderer.send('port-changed');
+        window.close();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("save").addEventListener("click", doNotify);
+})
